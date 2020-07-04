7ad5a639d58a672f814ebe03f0f598cffd8696ab
Fatemeh <fatemeh.gharri@yahoo.com>
2020-07-04 11:32:40 +0200

    Add security package

---
+++ app/src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+    public function supports(Request $request)
+    {
+        return $request->attributes->get('_route') === 'login'
+            && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {dd($request);
+        return [
+            'username' => $request->request->get('username'),
+            'password' => $request->request->get('password'),
+        ];
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        dd($credentials);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // todo
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // todo
+    }
+
+    /**
+     * Return the URL to the login page.
+     *
+     * @return string
+     */
+    protected function getLoginUrl()
+    {
+        // TODO: Implement getLoginUrl() method.
+    }
+}
