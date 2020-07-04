02dac0f87fa324028e314c5711786604c9ed4a97
Fatemeh <fatemeh.gharri@yahoo.com>
2020-07-04 11:30:41 +0200

    Define a login form

---
+++ app/src/Form/UserFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'username',
+                null,
+                [
+                    'label' => false,
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank(),
+                        new Length(['min' => 2, 'max' => 8]),
+                    ],
+                ]
+            )
+            ->add(
+                'password',
+                null,
+                [
+                    'label' => false,
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank(),
+                        new Length(['min' => 2, 'max' => 8]),
+                    ],
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+}
