fcd3c3209525d5300a7a5ab81d55e5a59930fc61
Fatemeh <fatemeh.gharri@yahoo.com>
2020-07-04 16:20:09 +0200

    Create user data with hashed passwords

--- app/src/DataFixtures/BaseFixture.php
+++ app/src/DataFixtures/BaseFixture.php
@@ -5,18 +5,19 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+
 abstract class BaseFixture extends Fixture
 {
-    private $manager;
-    protected $faker;
+    private $_manager;
+    protected $_faker;
 
 
     abstract protected function loadData(ObjectManager $manager);
 
     public function load(ObjectManager $manager)
     {
-        $this->manager = $manager;
-        $this->faker = Factory::create();
+        $this->_manager = $manager;
+        $this->_faker = Factory::create();
         $this->loadData($manager);
     }
 
@@ -27,7 +28,7 @@ abstract class BaseFixture extends Fixture
             if (null === $entity) {
                 throw new \LogicException('Did you forget to return the entity object from your callback to BaseFixture::createMany()?');
             }
-            $this->manager->persist($entity);
+            $this->_manager->persist($entity);
         }
     }
 }
--- app/src/DataFixtures/UserFixture.php
+++ app/src/DataFixtures/UserFixture.php
@@ -5,15 +5,27 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+    private $_passwordEncoder;
+
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->_passwordEncoder = $passwordEncoder;
+    }
+
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(10, function ($i) {
             $user = new User();
-            $user->setUsername($this->faker->userName);
-            $user->setPassword('');
+            $user->setUsername($this->_faker->userName);
+            $user->setPassword($this->_passwordEncoder->encodePassword(
+                $user,
+                'password'
+            ));
             return $user;
         });
 
