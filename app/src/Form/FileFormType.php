b2199ef70676a880d19840d29cac88cda930a7dc
Fatemeh <fatemeh.gharri@yahoo.com>
2020-07-06 16:22:43 +0200

    Add NotBlank as new constraint to FileForm

--- app/src/Form/FileFormType.php
+++ app/src/Form/FileFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class FileFormType extends AbstractType
 {
@@ -21,6 +22,7 @@ class FileFormType extends AbstractType
                     'mapped'   => false,
                     'required' => false,
                     'constraints' => [
+                        new NotBlank(),
                         new File([
                             'maxSize'  => '8M'
                         ])
