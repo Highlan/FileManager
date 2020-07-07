3b8bb956096ae0f6b7b5b3f2a42505aee6eebf06
Fatemeh <fatemeh.gharri@yahoo.com>
2020-07-07 12:49:49 +0200

    Remove unused classes

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -10,9 +10,7 @@ use App\Service\FileService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class MainController extends AbstractController
